8d0e6b1aa0fec99dcdd46757d1eb3cace0d2952b
DPorch10 <aintshit@DPorch10s-MacBook-Pro.local>
2024-06-08 16:26:19 -0400

    onclicks

--- react-couplesconnect/src/Components/LoginSignup/LoginSignup.jsx
+++ react-couplesconnect/src/Components/LoginSignup/LoginSignup.jsx
@@ -34,9 +34,9 @@ const LoginSignup = () => {
             </div>  
         </div>
         <div className="forgot-password">Lost Password? <span>Click Here</span></div>
-        <div className="submit-container"> </div>
-        <div className="submit">{action==="Login"?"submit gray": "submit"}<div/>
-        <div className="submit">{action==="Sign Up"?"submit gray": "submit"}<div/>
+        <div className="submit-container"> 
+        <div className={action==="Login"?"submit gray": "submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up<div/>
+        <div className={action==="Sign Up"?"submit gray": "submit"} onClick={()=>{setAction("Login")}}>Login<div/>
             
     </div>
     </div>
