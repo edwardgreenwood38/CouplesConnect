9dd97f4baea5290eaf919cfd404f0dab62ee9b51
DPorch10 <aintshit@DPorch10s-MacBook-Pro.local>
2024-06-08 16:04:09 -0400

    placeholder

--- react-couplesconnect/src/Components/LoginSignup/LoginSignup.jsx
+++ react-couplesconnect/src/Components/LoginSignup/LoginSignup.jsx
@@ -10,19 +10,34 @@ import password_icon from '../Assets/password.png'
 
 const LoginSignup = () => {
   return (
-    <div className='container'>
-        <div className= "header">
-            <div className="text"> SingUp </div>
-            <div className="underline"></div>
+    <div className='container'> 
+    <div className= "header">
+        <div className="text"> Sing Up </div>
+        <div className="underline"></div>
         </div>
+    
         <div className="inputs">
          <div className="input">
-          <img src="" alt="" />
-          <input type="text" />  
-            </div>   
+          <img src={user_icon} alt="" />
+          <input type="text" placeholder="Name"/>  
+            </div> 
+            <div className="input">
+          <img src={email_icon} alt="" />
+          <input type="email" placeholder="Email" />  
+            </div>  
+            <div className="input">
+          <img src={password_icon} alt="" />
+          <input type="password" placeholder="" />    
+            </div>  
         </div>
+        <div className="forgot-password">Lost Password? <span>Click Here</span></div>
+        <div className="submit-container"> </div>
+        <div className="submit">Sign Up<div/>
+        <div className="submit">Login<div/>
+            
     </div>
-  )
-}
+    </div>
+  );
+};
 
 export default LoginSignup
