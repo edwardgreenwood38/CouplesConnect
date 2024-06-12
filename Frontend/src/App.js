a0346e7f8d60d18a81d68e4bfe04c97dee45cf78
DPorch10 <aintshit@DPorch10s-MacBook-Pro.local>
2024-06-11 20:34:26 -0400

    events page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,13 @@
 //
 import './App.css';
 import Signup from './components/loginSignup/Signup'
+import Events from './components/Events/Events'
 
 function App() {
   return(
     <div>
       <Signup></Signup>
+      <Events></Events>
     </div>
   );
 }
---
+++ Frontend/src/components/Events/Events.jsx
@@ -0,0 +1,189 @@
+import "./Events.css";
+const Events = () => {
+    return (
+        <>
+       <div className="Eventbased container">
+        <div className="content">
+            <h6>Our Events are Based On</h6> 
+        </div>
+
+        <div className="row">
+        <div className="col-sm-3">
+            </div>
+
+            <div className="col-sm-3">
+            <div className="box">
+                <h6>Parent Night Out</h6>
+                <p>
+                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
+                </p>
+
+            </div>
+            </div>
+
+            <div className="col-sm-3">
+            <div className="box">
+                <h6>Parent Night Out</h6>
+                <p>
+                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
+                </p>
+
+            </div>
+            </div>
+
+            <div className="col-sm-3">
+            <div className="box">
+                <h6>Parent Night Out</h6>
+                <p>
+                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
+                </p>
+
+            </div>
+            </div>
+
+            <div className="col-sm-3">
+            <div className="box">
+                <h6>Parent Night Out</h6>
+                <p>
+                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
+                </p>
+
+            </div>
+            </div>
+
+            
+
+            {/*second row*/}
+
+            <div className="col-sm-3">
+            <div className="box">
+                <h6>Parent Night Out</h6>
+                <p>
+                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
+                </p>
+
+            </div>
+            </div>
+
+            <div className="col-sm-3">
+            <div className="box">
+                <h6>Parent Night Out</h6>
+                <p>
+                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
+                </p>
+
+            </div>
+            </div>
+
+            <div className="col-sm-3">
+            <div className="box">
+                <h6>Parent Night Out</h6>
+                <p>
+                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
+                </p>
+
+            </div>
+            </div>
+
+            <div className="col-sm-3">
+            <div className="box">
+                <h6>Parent Night Out</h6>
+                <p>
+                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
+                </p>
+
+            </div>
+            </div>
+
+           {/*third row*/}
+
+            <div className="col-sm-3">
+            <div className="box">
+                <h6>Parent Night Out</h6>
+                <p>
+                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
+                </p>
+
+            </div>
+            </div>
+
+            <div className="col-sm-3">
+            <div className="box">
+                <h6>Parent Night Out</h6>
+                <p>
+                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
+                </p>
+
+            </div>
+            </div>
+
+            <div className="col-sm-3">
+            <div className="box">
+                <h6>Parent Night Out</h6>
+                <p>
+                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
+                </p>
+
+            </div>
+            </div>
+
+            <div className="col-sm-3">
+            <div className="box">
+                <h6>Parent Night Out</h6>
+                <p>
+                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
+                </p>
+
+            </div>
+            </div>
+            
+            {/*fourth row*/}
+
+            <div className="col-sm-3">
+                <div className="box">
+    <h6>Parent Night Out</h6>
+    <p>
+       Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
+    </p>
+
+                    </div>
+                </div>
+
+            <div className="col-sm-3">
+            <div className="box">
+             <h6>Parent Night Out</h6>
+    <p>
+       Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
+    </p>
+
+</div>
+</div>
+
+<div className="col-sm-3">
+<div className="box">
+    <h6>Parent Night Out</h6>
+    <p>
+       Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
+    </p>
+
+</div>
+</div>
+
+<div className="col-sm-3">
+<div className="box">
+    <h6>Parent Night Out</h6>
+    <p>
+       Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
+    </p>
+
+</div>
+</div>
+
+</div>
+
+        </div>
+        </>
+    );
+};
+
+export default Events;
