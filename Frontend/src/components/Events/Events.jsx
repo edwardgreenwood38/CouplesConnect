2fcef12b24d525b7eef0ce3c58a139daabb5357f
DPorch10 <aintshit@DPorch10s-MacBook-Pro.local>
2024-06-13 06:58:27 -0400

    input

--- Frontend/src/components/Events/Events.jsx
+++ Frontend/src/components/Events/Events.jsx
@@ -1,187 +1,53 @@
 import "./Events.css";
 const Events = () => {
     return (
-        <>
-       <div className="Eventbased container">
-        <div className="content">
-            <h6>Our Events are Based On</h6> 
-        </div>
-
-        <div className="row">
-        <div className="col-sm-3">
-            </div>
-
-            <div className="col-sm-3">
-            <div className="box">
-                <h6>Parent Night Out</h6>
-                <p>
-                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
-                </p>
-
-            </div>
-            </div>
-
-            <div className="col-sm-3">
-            <div className="box">
-                <h6>Parent Night Out</h6>
-                <p>
-                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
-                </p>
-
-            </div>
-            </div>
-
-            <div className="col-sm-3">
-            <div className="box">
-                <h6>Parent Night Out</h6>
-                <p>
-                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
-                </p>
-
-            </div>
-            </div>
-
-            <div className="col-sm-3">
-            <div className="box">
-                <h6>Parent Night Out</h6>
-                <p>
-                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
-                </p>
-
-            </div>
-            </div>
-
-            
-
-            {/*second row*/}
-
-            <div className="col-sm-3">
-            <div className="box">
-                <h6>Parent Night Out</h6>
-                <p>
-                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
-                </p>
-
-            </div>
-            </div>
-
-            <div className="col-sm-3">
-            <div className="box">
-                <h6>Parent Night Out</h6>
-                <p>
-                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
-                </p>
-
-            </div>
-            </div>
-
-            <div className="col-sm-3">
-            <div className="box">
-                <h6>Parent Night Out</h6>
-                <p>
-                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
-                </p>
-
-            </div>
-            </div>
-
-            <div className="col-sm-3">
-            <div className="box">
-                <h6>Parent Night Out</h6>
-                <p>
-                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
-                </p>
-
-            </div>
-            </div>
-
-           {/*third row*/}
-
-            <div className="col-sm-3">
-            <div className="box">
-                <h6>Parent Night Out</h6>
-                <p>
-                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
-                </p>
-
-            </div>
-            </div>
-
-            <div className="col-sm-3">
-            <div className="box">
-                <h6>Parent Night Out</h6>
-                <p>
-                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
-                </p>
-
-            </div>
-            </div>
-
-            <div className="col-sm-3">
-            <div className="box">
-                <h6>Parent Night Out</h6>
-                <p>
-                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
-                </p>
-
-            </div>
-            </div>
-
-            <div className="col-sm-3">
-            <div className="box">
-                <h6>Parent Night Out</h6>
-                <p>
-                   Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
-                </p>
-
-            </div>
-            </div>
-            
-            {/*fourth row*/}
-
-            <div className="col-sm-3">
-                <div className="box">
-    <h6>Parent Night Out</h6>
-    <p>
-       Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
-    </p>
-
-                    </div>
+        <> 
+            <div className="EventBased container">
+                <div className="content">
+                    <h6>Our Events</h6>
                 </div>
 
-            <div className="col-sm-3">
-            <div className="box">
-             <h6>Parent Night Out</h6>
-    <p>
-       Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
-    </p>
+                <div className="row">
 
-</div>
-</div>
+                <div className="col-sm-3">
+                    <div className="box">
+                        <h6>Parents Night Out</h6>
+                        <p>
+                            Come Out and have fun on this Kids Free Friday! We will have our weekly event at the Bowling Alley as we bowl for three hours and play arcades game. Mix and mingle with our couples and laugh and have fun. 
+                        </p>
+                        </div>
+                        </div>    
 
-<div className="col-sm-3">
-<div className="box">
-    <h6>Parent Night Out</h6>
-    <p>
-       Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
-    </p>
+                        <div className="col-sm-3">
+                    <div className="box">
+                        <h6>5k Marathon</h6>
+                        <p>
+                            Come Out and have fun on this Kids Free Friday! We will have our weekly event at the Bowling Alley as we bowl for three hours and play arcades game. Mix and mingle with our couples and laugh and have fun. 
+                        </p>
+                        </div>
+                        </div>
 
-</div>
-</div>
+                        <div className="col-sm-3">
+                    <div className="box">
+                        <h6>Bar Crawl</h6>
+                        <p>
+                            Come Out and have fun on this Kids Free Friday! We will have our weekly event at the Bowling Alley as we bowl for three hours and play arcades game. Mix and mingle with our couples and laugh and have fun. 
+                        </p>
+                        </div>
+                        </div>
 
-<div className="col-sm-3">
-<div className="box">
-    <h6>Parent Night Out</h6>
-    <p>
-       Come Join us, as we have fun this Friday with no kids. Enjoy fun and games with one another, while not having to worry about your children. 
-    </p>
+                        <div className="col-sm-3">
+                    <div className="box">
+                        <h6>Day In The Park</h6>
+                        <p>
+                            Come Out and have fun on this Kids Free Friday! We will have our weekly event at the Bowling Alley as we bowl for three hours and play arcades game. Mix and mingle with our couples and laugh and have fun. 
+                        </p>
+                        </div>
+                        </div>
 
-</div>
-</div>
 
-</div>
-
-        </div>
+                </div>
+            </div>
         </>
     );
 };
