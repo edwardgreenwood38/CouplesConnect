7025f1c9984101081e5454ac2b5a23dd814e5fa7
DPorch10 <aintshit@DPorch10s-MacBook-Pro.local>
2024-06-13 07:13:35 -0400

    styling 2.0

--- Frontend/src/components/Events/Events.jsx
+++ Frontend/src/components/Events/Events.jsx
@@ -22,7 +22,7 @@ const Events = () => {
                     <div className="box">
                         <h6>5k Marathon</h6>
                         <p>
-                            Come Out and have fun on this Kids Free Friday! We will have our weekly event at the Bowling Alley as we bowl for three hours and play arcades game. Mix and mingle with our couples and laugh and have fun. 
+                            This Sunday we will have our Couples 5k Marathon, as you and your significant other Run 5k across the town just to enjoy the fresh air and stay in shape. Marathon starts at 9AM ET. 
                         </p>
                         </div>
                         </div>
@@ -31,7 +31,7 @@ const Events = () => {
                     <div className="box">
                         <h6>Bar Crawl</h6>
                         <p>
-                            Come Out and have fun on this Kids Free Friday! We will have our weekly event at the Bowling Alley as we bowl for three hours and play arcades game. Mix and mingle with our couples and laugh and have fun. 
+                            Long week? Kids have been going crazy at home? Come and enjoy a drink and food with live band as we go to "DP's Pub" Next Saturday to relax after a long eventful week.
                         </p>
                         </div>
                         </div>
@@ -40,7 +40,7 @@ const Events = () => {
                     <div className="box">
                         <h6>Day In The Park</h6>
                         <p>
-                            Come Out and have fun on this Kids Free Friday! We will have our weekly event at the Bowling Alley as we bowl for three hours and play arcades game. Mix and mingle with our couples and laugh and have fun. 
+                            There's nothing like having grass in between your toes. As we are meeting at the park for a Couples Connect to talk , play games , cookout on the grill, as well as enjoy music and each others company. 
                         </p>
                         </div>
                         </div>
