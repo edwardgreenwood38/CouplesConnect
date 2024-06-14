df651bdd4802b8e2c2de780b7ad20e668435c002
raymond88wang <raymond88wang@gmail.com>
2024-06-13 19:27:36 -0700

    add frontend

---
+++ frontend/src/gallery/Match.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+function Match() {
+    return (
+        <div>
+            {/* Mock Your Matches Page, need to add images */}
+            {/* GALLERY STARTS */}
+            <h2>Available Now</h2>
+            <div class="row">
+                <div class="column">
+                    <div class="couple">
+                        <img src="placeholder.png" alt="placeholder" />
+                        <h4>Bob & Amy</h4>
+                        <p>Lorem ipsum dolor...</p>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="couple">
+                        <img src="placeholder.png" alt="placeholder" />
+                        <h4>Chris & Betty</h4>
+                        <p>Lorem ipsum dolor...</p>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="couple">
+                        <img src="placeholder.png" alt="placeholder" />
+                        <h4>Jane & Amber</h4>
+                        <p>Lorem ipsum dolor...</p>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="couple">
+                        <img src="placeholder.png" alt="placeholder" />
+                        <h4>John & Mary</h4>
+                        <p>Lorem ipsum dolor...</p>
+                    </div>
+                </div>
+            </div>
+            <div class="couple">
+                <img src="placeholder.png" alt="placeholder" />
+                <h4>Tim & Shaniqua</h4>
+                <p>Lorem ipsum dolor...</p>
+            </div>
+            {/* GALLERY ENDS */}
+        </div>
+    )
+
+}
+
+export default Match;
