2d30c5d630a9f74f07db50873e8bfa05f2af3dc0
Raymond Wang <raymond.wang@slalom.com>
2024-06-13 19:17:00 -0700

    fix folder structure

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,52 @@
-//
-import './App.css';
-import Signup from './components/loginSignup/Signup'
-import Events from './components/Events/Events'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import Navigation from './components/Navigation'
+import Error404 from './components/error404'
+import GalleryIndex from './gallery/GalleryIndex'
+import Members from './gallery/Members'
+import Match from './gallery/Match'
+import SignIn from './users/SignIn'
+import RegistrationForm from './users/RegistrationForm'
+import CurrentUserProvider from './contexts/CurrentUser'
+
+
+
+
 
 function App() {
-  return(
-    <div>
-      <Signup/>
-      
-      <Events/>
-    </div>
+  return (
+    <CurrentUserProvider>
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route  path="/" element={<Home />} />
+          <Route  path="/sign-in" element={<SignIn />} />
+          <Route  path="/registrationForm" element={<RegistrationForm />} />
+          <Route  path="/gallery" element={<GalleryIndex />} />
+          <Route  path="/match" element={<Match />} />
+          <Route  path="/members" element={<Members />} />
+          <Route path="/error404" element={<Error404 />} />
+        </Routes>
+      </Router>
+    </CurrentUserProvider>
   );
 }
+
+
+
+        
+        
+
+
+  
+        
+
+
+
+
+  
+   
+
+
 
 export default App;
